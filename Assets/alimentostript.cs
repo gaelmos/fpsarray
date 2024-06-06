fffeba3dad7984314d983565cf921fc2b805f205
MOSZENBERG GAEL <48242494@ort>
2024-06-06 10:44:11 -0300

    terminado

--- Assets/alimentostript.cs
+++ Assets/alimentostript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class alimentostript : MonoBehaviour
 {
+    public int valoralimentario;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/personajescript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personajescript : MonoBehaviour
+{
+    public int nivelhambre;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void tomaralimento(int valoralimentario)
+    {
+        nivelhambre -= valoralimentario;
+        if (nivelhambre <= 0)
+        {
+            nivelhambre = 0;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        alimentostript alimento;
+        alimento = other.GetComponent<alimentostript>();
+        if (alimento)
+        {
+            tomaralimento(alimento.valoralimentario);
+            Destroy(other.gameObject);
+        }
+    }
+}
